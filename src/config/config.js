c702d2259f4efff763e44e64c01ca57cc057c8bb
Juanes <jedp082@gmail.com>
2020-11-01 13:28:01 -0500

    feature/connectionToRabbit

--- src/config/config.js
+++ src/config/config.js
@@ -8,6 +8,8 @@ const config = {
 	ENDPOINT_AUTH: 'https://api.twitter.com/oauth2/token',
 	ENDPOINT_GET_TWEETS: 'https://api.twitter.com/2/tweets/search/stream',
 	ENDPOINT_RULES: 'https://api.twitter.com/2/tweets/search/stream/rules',
+	RABBITMQ_HOST: process.env.RABBITMQ_HOST,
+	QUEUE_NAME: 'Tweets'
 }
 
 module.exports = config
---
+++ src/lib/publisher/index.js
@@ -0,0 +1,33 @@
+const amqp = require('amqplib')
+const { RABBITMQ_HOST, QUEUE_NAME } = require('../../config/config')
+
+const publisher = async (tweet) => {
+
+  const connection = await amqp.connect(RABBITMQ_HOST)
+  const channel = await connection.createChannel()
+
+  await channel.assertQueue(QUEUE_NAME)
+
+  const sent = channel.sendToQueue(QUEUE_NAME, Buffer.from(JSON.stringify(tweet)), {
+    persistent: true
+  })
+
+  if(sent) {
+    
+   return {
+      error: false,
+      message: `Tweet ${tweet} was send to the queue ${QUEUE_NAME}`,
+   }
+  } else {
+    connection.close()
+    return {
+      error: `Tweet ${tweet} wasn't send to the queue ${QUEUE_NAME}, something failed`,
+    }
+    process.exit(0)
+  }
+
+}
+
+module.exports = {
+  publisher
+}
