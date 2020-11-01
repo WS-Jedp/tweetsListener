5ce76602168bc694814f545df1d6233c1a70da73
Juanes <jedp082@gmail.com>
2020-11-01 16:52:33 -0500

    Conenction to redis done, only left api and GraphQl

--- src/lib/consumer/index.js
+++ src/lib/consumer/index.js
@@ -1,21 +1,20 @@
 const amqp = require('amqplib')
 const { RABBITMQ_HOST, QUEUE_NAME } = require('../../config/config')
+const { RedisClient } = require('../redis')
 
 const consumer = async () => {
 
-  let receivedTweet = {};
+  const redis = new RedisClient('Tweets')
 
   const connection = await amqp.connect(RABBITMQ_HOST)
   const channel = await connection.createChannel()
 
   await channel.assertQueue(QUEUE_NAME)
-
   await channel.consume(QUEUE_NAME, (message) => {
-    receivedTweet = JSON.parse(message.content)
-    channel.ack(tweet)
+    const content = JSON.parse(message.content)
+    redis.pushItem(content)
+    channel.ack(message)
   })
-
-  return receivedTweet;
 
 }
 
--- src/routes/tweets/index.js
+++ src/routes/tweets/index.js
@@ -9,13 +9,12 @@ const TweetsRouter = (app) => {
 	app.use(router)
 
 	router.get('/tweets', async (req, res, next) => {
-		const tokenData = await setToken()		
+		const tokenData = await setToken()
 		const tweets = await getTweets()
 		const lastTweet = tweets[tweets.length - 1]
 
-		const data = await publisher(tweets[tweets.length - 1]) 
+		const data = await publisher(tweets[tweets.length - 1])
 
-		console.log("")
 		data.error ? res.send(data.error) : res.send(data.message)
 	})
 }
